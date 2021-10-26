3dbede2aaac0ac0f7c72a7ef4244812c174c0e7f
“Ahmad <ahmadabotoimah@gmail,com>
2021-10-26 16:02:18 +0300

    finished

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(show_cupcakes);
 
 var cup_cakes = [
@@ -12,9 +13,8 @@ var cup_cakes = [
 function show_cupcakes() {
     //write code that shows the cupcakes in the table as per the instructions
 }
-
-
-
+const btn = document.getElementById('btn');
+const form = document.querySelector('form');
 
 
 // function validate(){
@@ -38,30 +38,32 @@ const customerFormat = /^[a-zA-Z]{5,16}$/;
 //         }
 //     }
 // };
-const customerValid = function () {
+const customerValid = function (e) {
     if (customerInput.value === "") {
         CustomerHelp.innerHTML = "you should fill this field";
         CustomerHelp.style.color = "red";
-    } else {
+        e.preventDefault();
         if (customerInput.value.match(customerFormat)) {
             CustomerHelp.innerHTML = "";
             CustomerHelp.style.color = "green";
+
         } else {
             CustomerHelp.innerHTML = "Customer Name is not valid";
             CustomerHelp.style.color = "red";
         }
     }
 };
-customerInput.addEventListener('keyup', customerValid);
+btn.addEventListener('click', customerValid);
 
 //count valid:
 const countInput = document.getElementById('count-Input');
 const countHelp = document.getElementById('count-Help');
 const countFormat = /^[0-9\.]{1,15}$/;
-const countValid = function () {
+const countValid = function (e) {
     if (countInput.value === "") {
         countHelp.innerHTML = "you should fill this field";
         countHelp.style.color = "red";
+        e.preventDefault();
     } else {
         if (countInput.value.match(countFormat)) {
             countHelp.innerHTML = " ";
@@ -69,39 +71,132 @@ const countValid = function () {
         } else {
             countHelp.innerHTML = "count longer than needs";
             countHelp.style.color = "red";
+            e.preventDefault();
         }
     }
 };
-countInput.addEventListener('keyup', countValid);
+btn.addEventListener('click', countValid);
 
 //type:
 const typeInput = document.getElementById('type-input');
-const typeValid = function () {
+const typeValid = function (e) {
     console.log(typeInput.value);
     if (typeInput.value === 'none') {
+        typeInput.style.border = 'solid 2px red';
         document.getElementById('type-help').style.color = 'red';
         document.getElementById('type-help').style.marginTop = '-15px';
+        document.getElementById('type-help').style.fontWeight = 'bold';
         document.getElementById('type-help').innerText = 'you cant choose None ';
+        e.preventDefault();
+    } else {
+        typeInput.style.border = 'solid 2px green';
+        document.getElementById('type-help').innerText = '';
     }
 };
-typeInput.addEventListener('change', typeValid);
+btn.addEventListener('click', typeValid);
 
 //delivery:
 const dileveryInput = document.getElementById('delivery-input');
-const dileveryValid = function () {
+const dileveryValid = function (e) {
     console.log(dileveryInput.value);
     if (dileveryInput.value === 'none') {
+        dileveryInput.style.border = 'solid 2px red';
         document.getElementById('dilevery-help').style.color = 'red';
         document.getElementById('dilevery-help').style.marginTop = '-15px';
+        document.getElementById('dilevery-help').style.fontWeight = 'bold';
         document.getElementById('dilevery-help').innerText = 'you cant choose None ';
+        e.preventDefault();
+    } else {
+        dileveryInput.style.border = 'solid 2px green';
+        document.getElementById('dilevery-help').innerText = '';
+    }
+    if (dileveryInput.value == '4' && typeInput.value == 'chocolate') {
+        dileveryInput.style.border = 'solid 2px red';
+        typeInput.style.border = 'solid 2px red';
+        document.getElementById('type-help').style.color = 'red';
+        document.getElementById('type-help').style.marginTop = '-20px';
+        document.getElementById('type-help').style.fontWeight = 'bold';
+        document.getElementById('type-help').innerText = 'This type of cake cannot be delivered at 4 PM';
+        e.preventDefault();
     }
 };
-dileveryInput.addEventListener('change', dileveryValid);
+btn.addEventListener('click', dileveryValid);
+
+//conditions of free:
+
+console.log(form);
+const algInput = document.getElementById('allergies-input');
+
+const freeCheck = function (e) {
+    if (typeInput.value == 'chocolate' && algInput.value == 'dairy') {
+        typeInput.style.border = 'solid 2px red';
+        algInput.style.border = 'solid 2px red';
+        document.getElementById('free').style.color = 'red';
+        document.getElementById('free').style.fontWeight = 'bold';
+        document.getElementById('free').style.marginTop = '-18';
+        document.getElementById('free').innerText = 'Chocolate is not Dairy free';
+        e.preventDefault();
+
+    } else {
+        document.getElementById('free').innerHTML = '';
+    }
+    if (typeInput.value == 'pecan' && algInput.value == 'nut') {
+        typeInput.style.border = ' solid 2px red';
+        algInput.style.border = 'solid 2px red';
+        document.getElementById('free').style.color = 'red';
+        document.getElementById('free').style.fontWeight = 'bold';
+        document.getElementById('free').style.marginTop = '-18';
+        document.getElementById('free').innerText = 'Pecan is not  Nut free';
+        e.preventDefault();
+
+    } else {
+        {
+            document.getElementById('free').innerText = '';
+        }
+    }
+};
+btn.addEventListener('click', freeCheck);
+
+
 
 // }
 
+//table 
+document.getElementsByTagName('td')[0].innerHTML = `${cup_cakes[0].name}`;
+document.getElementsByTagName('td')[1].innerHTML = `${cup_cakes[0].calories}`;
+document.getElementsByTagName('td')[2].innerHTML = `${cup_cakes[0].weight}`;
+document.getElementsByTagName('td')[3].innerHTML = `${cup_cakes[1].name}`;
+document.getElementsByTagName('td')[4].innerHTML = `${cup_cakes[1].calories}`;
+document.getElementsByTagName('td')[5].innerHTML = `${cup_cakes[1].weight}`;
+document.getElementsByTagName('td')[6].innerHTML = `${cup_cakes[2].name}`;
+document.getElementsByTagName('td')[7].innerHTML = `${cup_cakes[2].calories}`;
+document.getElementsByTagName('td')[8].innerHTML = `${cup_cakes[2].weight}`;
+document.getElementsByTagName('td')[9].innerHTML = `${cup_cakes[3].name}`;
+document.getElementsByTagName('td')[10].innerHTML = `${cup_cakes[3].calories}`;
+document.getElementsByTagName('td')[11].innerHTML = `${cup_cakes[3].weight}`;
+document.getElementsByTagName('td')[12].innerHTML = `${cup_cakes[4].name}`;
+document.getElementsByTagName('td')[13].innerHTML = `${cup_cakes[4].calories}`;
+document.getElementsByTagName('td')[14].innerHTML = `${cup_cakes[4].weight}`;
+
+for (i = 0; i < 15; i++) {
+    if (document.getElementsByTagName('td')[i].innerText == 'high') {
+        document.getElementsByTagName('td')[i].style.backgroundColor = 'red';
+    } else if (document.getElementsByTagName('td')[i].innerText == 'medium') {
+        document.getElementsByTagName('td')[i].style.backgroundColor = 'orange';
+    } else if (document.getElementsByTagName('td')[i].innerText == 'low') {
+        document.getElementsByTagName('td')[i].style.backgroundColor = 'green';
+    }
+}
 function show_storage() {
     //write code that shows the name from local storage
 }
+const welcome = document.getElementById('welcome');
+const welcomeuser = function () {
+    localStorage.setItem('userName', customerInput.value);
+
+};
+form.addEventListener('submit', welcomeuser, welcome.innerHTML = `${localStorage.getItem('userName')}`);
+
+
 
 
